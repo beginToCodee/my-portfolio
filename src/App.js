b255f0c7f3eddc658ca9d984e91e5eed0f61fe26
sirjan <sirjantmg99@gmail.com>
2021-06-08 13:04:11 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React,{useEffect} from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import {Switch,Route} from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Skill from './components/Skill';
+import Service from './components/Services';
+import Project from './components/Project';
+
+
+
+
+
+
+function App() {
+  useEffect(()=>{
+    console.log(window.location.pathname)
+  },[]);
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/my-portfolio" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/skill" component={Skill} />
+        <Route path="/service" component={Service} />
+        <Route path="/project" component={Project} />
+      </Switch>
+     </div>
+  );
+}
+
+export default App;
